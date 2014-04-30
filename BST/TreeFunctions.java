a3a381db5b8c50109a7f3e169019c5f760c0ab07
chinmaykini <chinmaykini@gmail.com>
2014-04-29 20:38:14 -0400

    tree

---
+++ BST/TreeFunctions.java
@@ -0,0 +1,43 @@
+package com.ck.tree.service;
+
+import java.util.ArrayList;
+
+import com.ck.tree.model.TreeNode;
+
+public interface TreeFunctions {
+	
+	public String BFS(TreeNode root);
+	
+	public String DFS(TreeNode root);
+
+	String inorder(TreeNode node);
+	
+	ArrayList<ArrayList<Integer>> levelOrderTraversal(TreeNode node);
+	
+	public TreeNode rotateUpsideDown(TreeNode root);
+	
+	public TreeNode trimBST(TreeNode root, int min, int max);
+	
+	public TreeNode BstToSingleLinkedList(TreeNode root);
+	
+	public TreeNode BstToDoubleLinkedList(TreeNode root);
+	
+	public TreeNode BstToDoubleCircularLinkedList(TreeNode root);
+
+	TreeNode deleteLeaves(TreeNode root);
+
+	int getTreeHeight(TreeNode root);
+
+	void traverseSpiralRecursive(TreeNode root);
+
+	TreeNode lowestCommmonAncestor(TreeNode root, int p, int q);
+
+	TreeNode treeRightRotate(TreeNode root);
+
+	int sizeTree(TreeNode root);
+
+	void traverseSpiralNonRecursive(TreeNode root);
+
+	void printNodesInRange(TreeNode root, int min, int max);
+	
+}
